7d1a1db3192a6a8ec5588aa3c586e20daf9784fc
Sthadip <46148822+Sthadip@users.noreply.github.com>
2019-01-08 13:04:43 +0545

    First commit

---
+++ app/CrmStatus.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CrmStatus
+ *
+ * @package App
+ * @property string $title
+*/
+class CrmStatus extends Model
+{
+    protected $fillable = ['title'];
+    protected $hidden = [];
+    
+    
+    public static function boot()
+    {
+        parent::boot();
+
+        CrmStatus::observe(new \App\Observers\UserActionsObserver);
+    }
+    
+}
---
+++ database/migrations/2018_12_12_131059_add_5c10ecc42fd6f_relationships_to_crmcustomer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Add5c10ecc42fd6fRelationshipsToCrmCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('crm_customers', function(Blueprint $table) {
+            if (!Schema::hasColumn('crm_customers', 'crm_status_id')) {
+                $table->integer('crm_status_id')->unsigned()->nullable();
+                $table->foreign('crm_status_id', '239817_5c10ecc2a7ec5')->references('id')->on('crm_statuses')->onDelete('cascade');
+                }
+                
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('crm_customers', function(Blueprint $table) {
+            
+        });
+    }
+}
